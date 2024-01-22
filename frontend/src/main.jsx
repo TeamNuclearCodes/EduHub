8ece1a7c931a1e864319a93094f018ba63abbc40
1337kid <45532566+1337kid@users.noreply.github.com>
2024-01-22 20:56:47 +0530

    setup routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Forum from './pages/Forum.jsx'
+import Question from './components/Question.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    children: [
+      {
+        path:'forum',
+        element: <Forum/>
+      },
+      {
+        path:'forum/:slug',
+        element: <Question/>
+      }
+    ]
+  }
+])
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
