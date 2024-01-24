51f65be183604317f3c3b85a323895e55a9c57c1
1337kid <45532566+1337kid@users.noreply.github.com>
2024-01-24 13:31:27 +0530

    idk

--- frontend/src/pages/Questions/Ask.jsx
+++ frontend/src/pages/Questions/Ask.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
-import {ProtectedRoute, Button} from "../../components"
+import { useState, useEffect } from "react"
+import {ProtectedRoute, Button, HrtLn} from "../../components"
 import {useNavigate} from 'react-router-dom'
 import getAuth from "../../utils/getAuth"
 
 const Ask = () => {
   const [question,setQuestion] = useState({question:""})
+  const [questions,setQuestions] = useState([])
   const user = getAuth()
   const navigate = useNavigate()
 
@@ -24,19 +25,25 @@ const Ask = () => {
     })
   }
 
+  useEffect(() => {
+    fetch('http://localhost:5000/api/questions').then(res => res.json()).then(
+      data => setQuestions(data)
+    )
+  },[])
+
+
   return (
     <ProtectedRoute>
       <div className='container p-4'>
-
-          <form className='flex flex-col justify-center items-center gap-3' onSubmit={handleSubmit}>
-            <h3 className='text-xl'>Ask a Question</h3>
-            <textarea required rows={10} placeholder='Wriet your question here' className='form__input'
-              value={question.question}
-              onChange={(e) => setQuestion({...question,question: e.target.value})}
-            />
-            <Button type="submit" text="Submit" variant="black"/>
-          </form>
-
+        <form className='flex flex-col justify-center items-center gap-3 pb-2' onSubmit={handleSubmit}>
+          <h3 className='text-xl'>Ask a Question</h3>
+          <textarea required rows={10} placeholder='Wriet your question here' className='form__input'
+            value={question.question}
+            onChange={(e) => setQuestion({...question,question: e.target.value})}
+          />
+          <Button type="submit" text="Submit" variant="black"/>
+        </form>
+        <HrtLn/>
       </div>
     </ProtectedRoute>
   )
