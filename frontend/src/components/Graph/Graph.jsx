130404371bfa19350806ee8610c8704eab7b12f5
1337kid <45532566+1337kid@users.noreply.github.com>
2024-02-11 21:46:18 +0530

    dashboard - "your progress" above graph

--- frontend/src/components/Graph/Graph.jsx
+++ frontend/src/components/Graph/Graph.jsx
@@ -5,13 +5,15 @@ import { Chart as ChartJS, defaults } from "chart.js/auto";
 
 const Graph = ({ data }) => {
   return (
-    <div className="shadow-[2px_2px_4px_rgba(0,0,0,0.2)] bg-zinc-950 rounded-lg">
-      <Line data={data}
-        options= {{
-          borderColor:'#fff',
-          color: '#fff'
-        }}
-      />
+    <div className="bg-gradient p-[1px] rounded-lg">
+      <div className="shadow-[2px_2px_4px_rgba(0,0,0,0.2)] bg-zinc-950 rounded-lg">
+        <Line data={data}
+          options= {{
+            borderColor:'#fff',
+            color: '#fff'
+          }}
+        />
+      </div>
     </div>
   );
 };
