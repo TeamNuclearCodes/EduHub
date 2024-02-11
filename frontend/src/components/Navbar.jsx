4ef23ba1f786ccb294eabf570083a2d759523e96
1337kid <45532566+1337kid@users.noreply.github.com>
2024-02-11 15:46:36 +0530

    navbar update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 import { navbarLinks } from "../constants"
 import getAuth from '../utils/getAuth'
 import { Button } from  '.'
@@ -13,17 +13,19 @@ const Navbar = () => {
 
   return (
     <div className="bg-gradient justify-center flex">
-      <div className='container text-white py-2 flex justify-between max-md:px-2'>
+      <div className='container text-white py-2 flex justify-between max-md:px-2 items-center'>
         <div className="flex gap-3 align-middle items-center text-black">
-              <img src="/logo.png" width="100px"/>
-          <div className='flex gap-3'>
+            <Link to="/">
+              <img src="/logo.png" width="140px"/>
+            </Link>
+          <div className='flex gap-1 items-center'>
             {navbarLinks.map((link) => (
               <NavLink to={link.link} key={link.link}
                 className={({isActive}) => {
                   if(isActive) {
-                    return 'bg-zinc-950 py-1 px-2 rounded-full font-[600] focus:rounded-full text-[#681faa]'
+                    return 'bg-zinc-950 py-1 px-2 flex items-center rounded-full font-[600] focus:rounded-full text-[#681faa]'
                   } else {
-                    return 'text-purple-200 hover:text-zinc-900 font-[500] py-1 px-2 rounded-full focus:rounded-full'
+                    return 'text-purple-200 flex items-center hover:text-zinc-900 font-[500] py-1 px-2 rounded-full focus:rounded-full'
                   }
               }}
               >
