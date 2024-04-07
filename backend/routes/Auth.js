408bc985b7ae5a181a14020177279b94ce9fe967
1337kid <45532566+1337kid@users.noreply.github.com>
2024-04-07 12:35:47 +0530

    fix: user not able to login after creating an account

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -34,22 +34,24 @@ router.post('/login',async (req,res) => {
 router.post('/signup', async(req,res) => {
     try {
         const body = req.body
-        let user = await User.findOne({username: body.username})
+        const user = await User.findOne({username: body.username})
         if (user) res.json({error: 'Username is taken. Pick another one'}).status(409)
         else {
-            user = new User({
+            const newUser = new User({
                 username: body.username,
                 password: bcrypt.hashSync(body.password, bcrypt.genSaltSync(10)),
                 name: body.name,
                 college: body.college,
                 semester: body.semester
             })
-            user.save()
-            const jwtoken = genToken(user)
-            delete user._doc.password
+            newUser.save()
+            const jwtoken = genToken(newUser)
+            let returnobj = {...newUser._doc}
+            delete returnobj.password
+            returnobj.token = jwtoken
             res.json({
                 message: "SignUp Successful",
-                user: user
+                user: returnobj
             }).status(201)
         }
     } catch (error) {
