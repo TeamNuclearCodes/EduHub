9fa4f9c9b32e5b89ae10197ef658bbadc468a6ab
midhununni457 <midhununni457@gmail.com>
2024-04-13 21:36:17 +0530

    search field frontend

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,10 +5,23 @@ import CommentCard from "./Question/CommentCard";
 import Button from "./Button";
 import HrtLn from "./HrtLn";
 import Question from "./Question/Question";
-import Task from './ToDo/Task';
+import Task from "./ToDo/Task";
 import AlertCard from "./AlertCard";
 import ProfileNavbar from "./Profile/ProfileNavbar";
 import UserCard from "./Profile/UserCard";
 import Tag from "./Question/Tag";
 
-export {Navbar ,Footer ,ProtectedRoute ,CommentCard ,Button, HrtLn, Question, Tag, Task, AlertCard, ProfileNavbar, UserCard}
+export {
+  Navbar,
+  Footer,
+  ProtectedRoute,
+  CommentCard,
+  Button,
+  HrtLn,
+  Question,
+  Tag,
+  Task,
+  AlertCard,
+  ProfileNavbar,
+  UserCard,
+};
--- frontend/src/pages/Questions/Ask.jsx
+++ frontend/src/pages/Questions/Ask.jsx
@@ -11,6 +11,7 @@ const Ask = () => {
   const [questions, setQuestions] = useState([]);
   const [tags, setTags] = useState([]);
   const [tag, setTag] = useState({ tag: "" });
+  const [searchQuery, setSearchQuery] = useState({ query: "" });
   const [userQuestions, setUserQuestions] = useState([]);
   const navigate = useNavigate();
 
@@ -33,6 +34,10 @@ const Ask = () => {
       });
   };
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+
   const handleTagSubmit = (e) => {
     e.preventDefault();
     if (tag.tag) {
@@ -41,8 +46,8 @@ const Ask = () => {
     }
   };
 
-  const handleTagDelete = (index,e) => {
-    e.preventDefault()
+  const handleTagDelete = (index, e) => {
+    e.preventDefault();
     setTags((ogTags) => {
       const newTags = [...ogTags];
       newTags.splice(index, 1);
@@ -58,7 +63,7 @@ const Ask = () => {
     })
       .then((res) => res.json())
       .then((data) => setUserQuestions(data));
-  },[]);
+  }, []);
 
   useEffect(() => {
     fetch(`${apiBase}/api/questions`)
@@ -69,6 +74,20 @@ const Ask = () => {
   return (
     <ProtectedRoute>
       <div className="container p-4 flex gap-2">
+        {/* Search Field */}
+        <div>
+          <form onSubmit={(e) => handleSearch(e)}>
+            <input
+              type="text"
+              value={searchQuery.query}
+              onChange={(e) =>
+                setSearchQuery({ ...searchQuery, query: e.target.value })
+              }
+            />
+            <button>Search</button>
+          </form>
+        </div>
+        {/* Add Question */}
         <div className="flex flex-col w-8/12">
           <form
             className="flex flex-col justify-center gap-2 pb-2"
@@ -86,10 +105,17 @@ const Ask = () => {
                 setQuestion({ ...question, question: e.target.value })
               }
             />
+            {/* Add Tags */}
             <div className="flex gap-2">
-              {tags.length > 0 ? tags.map((tag, index) => (
-                  <Tag tag={tag} index={index} handleDelete={handleTagDelete}></Tag>
-              )): (
+              {tags.length > 0 ? (
+                tags.map((tag, index) => (
+                  <Tag
+                    tag={tag}
+                    index={index}
+                    handleDelete={handleTagDelete}
+                  ></Tag>
+                ))
+              ) : (
                 <span className="text-sm text-zinc-400">No tags added</span>
               )}
             </div>
@@ -101,9 +127,14 @@ const Ask = () => {
                 value={tag.tag}
                 onChange={(e) => setTag({ tag: e.target.value })}
               />
-              <button className="w-5/12 flex justify-center items-center bg-zinc-600 rounded-md px-2 hover:bg-zinc-700" onClick={handleTagSubmit}><MdAdd/>Add Tag</button>
+              <button
+                className="w-5/12 flex justify-center items-center bg-zinc-600 rounded-md px-2 hover:bg-zinc-700"
+                onClick={handleTagSubmit}
+              >
+                <MdAdd />
+                Add Tag
+              </button>
             </div>
-
             <Button
               type="submit"
               text="Submit Question"
@@ -112,6 +143,7 @@ const Ask = () => {
             />
           </form>
           <HrtLn />
+          {/* Recent Questions Section */}
           <div className="p-2 flex flex-col gap-2">
             <h4 className="text-[20px] underline underline-offset-8 pb-2">
               Recent Questions
@@ -121,6 +153,7 @@ const Ask = () => {
             ))}
           </div>
         </div>
+        {/* Users Questions Section */}
         <div className="flex flex-col w-4/12 gap-2">
           <h2 className="text-3xl bg-clip-text bg-gradient text-transparent max-xl:w-full font-[500] w-8/12 mb-3">
             Your Questions
